9329551b88ca445db8b36af0275221ad25923202
danielevergani <dvergani94@gmail.com>
2021-03-25 15:33:45 +0100

    creato array di immagini e indice corrispondente

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 var app = new Vue ({
     el: "#root",
     data: {
-        
+        images: [
+            'https://i.pinimg.com/originals/ca/ca/aa/cacaaa6ed79406c476d60a60441ca502.jpg',
+            'https://www.gorgolive.it/wp-content/uploads/2019/12/canada-2020.jpg',
+            'https://images.lonelyplanetitalia.it/uploads/shutterstock-185800496?q=80&p=slider&s=4d256dfd284af350d8605473326767f9'
+        ],
+        imagesIndex: 1
     }
 })
